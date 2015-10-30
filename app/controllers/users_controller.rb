821076ad170e61afe82e16a61a9cff92d42e4258
mugisha <mugishaj@outlook.com>
2015-10-30 18:44:12 +0000

    remove unnecessary override in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,18 +27,7 @@ class UsersController < Clearance::UsersController
 
   private
 
-
-  def user_from_params
-    email = user_params.delete(:email)
-    password = user_params.delete(:password)
-
-	Clearance.configuration.user_model.new(user_params).tap do |user|
-	  user.email = email
-	  user.password = password
-	end
-
-  end
-
+  #Mugisha: override user_params to extract only email and password from params hash
   def user_params
     params.require(:user).permit(:email, :password) || Hash.new
   end
