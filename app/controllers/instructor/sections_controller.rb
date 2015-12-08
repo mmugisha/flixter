299e89b3b6164ca8005ee1881496a112bc31f04f
Mugisha <mugishaj@outlook.com>
2015-12-08 20:16:49 +0000

    add sections to the instructor course show page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,4 +1,21 @@
 class Instructor::SectionsController < ApplicationController
 	def new
+		@course = Course.find(params[:course_id])
+		@section = Section.new
+	end
+
+	def create
+		@course = Course.find(params[:course_id])
+		@section = @course.sections.create(section_params)
+		if @section.valid?
+			redirect_to instructor_course_path(@course)
+		else
+			render :url => new_instructor_course_section(@course)
+		end
+	end
+
+	private
+	def section_params
+		params.require(:section).permit(:title)
 	end
 end
