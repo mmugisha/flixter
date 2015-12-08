e1383239aa0e438dd9210adaec390411b7be96dd
Mugisha <mugishaj@outlook.com>
2015-12-08 03:25:11 +0000

    restart flixter development

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   include Clearance::User
   has_many :courses
-  validates :password, confirmation: true
+  validates :password, confirmation: true, presence:true
   validates :password_confirmation, presence: true
 end
