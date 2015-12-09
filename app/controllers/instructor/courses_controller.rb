344d1fb6007e18f8ec45c69bc101a16c5017d5fd
Mugisha <mugishaj@outlook.com>
2015-12-09 19:24:24 +0000

    secure the course show page so that only instructor who created it can view it

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,6 @@
 class Instructor::CoursesController < ApplicationController
 	before_action :require_login
+	before_action :require_authorized_for_current_course, :only=>[:show]
 
 	def new
 		@course = Course.new
@@ -16,11 +17,15 @@ class Instructor::CoursesController < ApplicationController
 	end
 
 	def show
-		@course = Course.find(params[:id])
 	end
 
 	private
-	
+	def require_authorized_for_current_course
+		if current_course.user != current_user
+			render :text => "Unauthorized", :status => :Unauthorized
+		end
+	end
+
 	helper_method :current_course
 	def current_course
 		@current_course ||= Course.find(params[:id])
