b43d3d6a30d526a143ffc3f8c5f7bf465d4e0c53
Mugisha <mugishaj@outlook.com>
2015-12-15 20:44:02 +0000

    make the instructor course page beautiful:

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -38,7 +38,7 @@ class Instructor::LessonsController < ApplicationController
 	end
 
 	def lesson_params
-		params.require(:lesson).permit(:title, :subtitle, :video,:row_order)
+		params.require(:lesson).permit(:title, :subtitle, :video,:row_order_position)
 	end
 
 	helper_method :current_section
