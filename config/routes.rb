5308788da09831185692296f1952450ccfc240c7
Mugisha <mugishaj@outlook.com>
2015-12-17 09:17:14 +0000

    add privacy policy page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Flixter::Application.routes.draw do
   root 'static_pages#index'
+  get 'privacy', :to=> 'static_pages#privacy'
   resource :dashboard, :only =>[:show]
   resources :courses, :only => [:index, :show] do 
     resources :enrollments, :only=> :create
